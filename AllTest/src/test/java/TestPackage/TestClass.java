491974deb16eb750e06f69e54f8098fe0eab7f99
Genzebgne <33294544+Genzebgne@users.noreply.github.com>
2018-06-03 17:02:33 -0400

    secodn commit

--- AllTest/src/test/java/TestPackage/TestClass.java
+++ AllTest/src/test/java/TestPackage/TestClass.java
@@ -11,7 +11,6 @@ public class TestClass extends HomePageClass {
 	@Test(priority=1)
 	    public void HomePageTestMethod() throws Exception {
 		HomePageClass.HomePageMethod();
-		
 		Thread.sleep(5000);
 		SharedFileClass.SharedMethod();
 		Thread.sleep(3000);
